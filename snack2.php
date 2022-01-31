5c81780ca7a13d8358189e907698b78fa1b772b6
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2022-01-31 16:18:46 +0100

    3 not done yet

--- snack2.php
+++ snack2.php
@@ -1,1 +1,36 @@
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+    
+
+<?php 
+
+$name = $_GET["name"];
+$nameLength = strlen($name);
+$mail = $_GET["mail"];
+$mailContent = strpos("@", $mail);
+$age = $_GET["age"];
+$isNumber = is_numeric($age);
+
+
+if (strlen($name) > 3 && strpos($mail, '@') == true && is_numeric($age) == true ) {
+    $message = "Accesso Riuscito";
+} else {
+    $message = "Accesso Negato";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1><?php echo $message ?></h1>
+    </div>
+</body>
+</html>
--- snack5.php
+++ snack5.php
@@ -1,1 +1,33 @@
 <!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
+
+<?php 
+
+$bigPara = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolores excepturi perferendis voluptate dignissimos ratione autem minima, cumque vero eveniet tempore sit praesentium, dolorem omnis sapiente eius cum fuga aut! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, nam voluptatibus, aliquid perferendis incidunt modi illum suscipit accusamus atque animi exercitationem optio nulla, ullam quas blanditiis iusto pariatur ea tempora. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam debitis vitae amet voluptatem aliquid perspiciatis nesciunt placeat illum ipsa quidem quo, quas, impedit, aliquam cumque aspernatur facilis odio? Accusantium, voluptatibus.";
+
+$explodedPara = explode(".", $bigPara);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+            echo($bigPara);
+            var_dump($explodedPara);
+
+            // for ($i=0; $i < count($explodedPara); $i++) { 
+            //     echo ($explodedPara[i]);
+            // }   
+            
+        ?>
+    </p>
+</body>
+</html>
